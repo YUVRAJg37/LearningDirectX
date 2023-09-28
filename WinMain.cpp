1241f20d1be4917f9d60703c07a93efea219f32c
yuvi <yuvraj.yash2001@gmail.com>
2023-09-29 01:52:08 +0530

    Window Ceated

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,10 +1,72 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE :
+		PostQuitMessage(10);
+		break;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPSTR	  lpCmdLine,
 	int		  nCmdShow 	)
 {
+	const auto pClassName = L"DirectX";
+
+	//Create Window Class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+
+	//Register Window Class
+	RegisterClassEx(&wc);
+
+	//Create Window Instance
+	HWND hWnd = CreateWindowEx(
+		0, pClassName,
+		L"DirectX Window",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr, nullptr, hInstance, nullptr
+	);
+
+	//Show the Window
+	ShowWindow(hWnd, SW_SHOW);
+
+	//Message
+	MSG msg;
+	BOOL gResult;
+	while( (gResult =  GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if(gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
+
 	return 0;
 }
